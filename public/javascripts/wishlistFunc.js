609700949957971f6b3a7fc8bc9284eb945cbb69
bseifert <bseifert@senacor.com>
2018-11-01 12:51:15 +0100

    show modal before publishing a wishlist

--- public/javascripts/wishlistFunc.js
+++ public/javascripts/wishlistFunc.js
@@ -35,8 +35,46 @@ function commentKeyupEvent(uuid) {
     }
 }
 
-function showPubshlishPopup(wishlistId) {
-    // todo show popup and then publishWishlist
+function showPubshlishModal(wishlistId) {
+    // initialize modal element
+    let modalEl = document.createElement('div');
+    modalEl.style.width = '400px';
+    modalEl.style.height = '400px';
+    modalEl.style.margin = '100px auto';
+    modalEl.style.backgroundColor = '#fff';
+    modalEl.style.textAlign = "center";
+    modalEl.style.padding = "20px";
+
+    let par1 = document.createElement("p");
+    par1.style.fontSize = "30px";
+    par1.innerHTML = "Are you sure you want to publish this wishlist?";
+    let par2 = document.createElement("p");
+    par2.style.fontSize = "30px";
+    par2.innerHTML = "Once published a wishlist cannot be edited anymore.";
+    let okButton = document.createElement("button");
+    okButton.className = "mui-btn mui-btn--primary";
+    okButton.innerHTML = "ok";
+    okButton.name = "okButton";
+    okButton.onclick = function () {
+        publishWishlist(wishlistId);
+        mui.overlay('off');
+    };
+
+    let cancelButton = document.createElement("button");
+    cancelButton.className = "mui-btn mui-btn--primary";
+    cancelButton.innerHTML = "cancel";
+    cancelButton.name = "cancelButton";
+    cancelButton.onclick = function () {
+        mui.overlay('off');
+    };
+
+    modalEl.appendChild(par1);
+    modalEl.appendChild(par2);
+    modalEl.appendChild(okButton);
+    modalEl.appendChild(cancelButton);
+
+    // show modal
+    mui.overlay('on', modalEl);
 }
 
 function publishWishlist(wishlistId) {
